7b4e1f841874e2c4032b67afc17c6220e5ddbf19
Andrius Razinskas <andrius.razinskas@juvare.com>
2025-02-08 17:47:52 +0200

    Initial solution template + added swagger for easier manual testing

---
+++ Accounts.WebApi/Program.cs
@@ -0,0 +1,26 @@
+
+namespace Accounts.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            app.MapOpenApi();
+            app.UseSwaggerUi(options =>
+            {
+                options.DocumentPath = "openapi/v1.json";
+            });
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
