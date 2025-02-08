5f3421e63604b7af27c4a258ba56ef4262f8323e
Andrius Razinskas <andrius.razinskas@juvare.com>
2025-02-08 19:37:05 +0200

    Add initial endpoint for accounts import

---
+++ Accounts.WebApi/V1/AccountsImport/AccountsImportController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Accounts.WebApi.V1.AccountsImport
+{
+    [ApiController]
+    [Route("api/v1/accounts/import")]
+    public class AccountsImportController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> ImportAccountsAsync(IFormFile file, CancellationToken cancellationToken)
+        {
+            return Created();
+        }
+    }
+}
